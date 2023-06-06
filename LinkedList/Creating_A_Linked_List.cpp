f186a36df4ae8149d53efaf8d8035f799a73a058
Atharva Joshi <86041881+Atharva1583@users.noreply.github.com>
2023-06-06 15:07:33 +0530

    Create Creating_A_Linked_List.cpp
    Creating a linked list from an array

---
+++ LinkedList/Creating_A_Linked_List.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*class Node {
+public:
+    int data;
+    Node* next;
+  
+    // Default constructor
+    Node()
+    {
+        data = 0;   
+        next = NULL;
+    }
+  
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+void insert(Node* &head,int d){
+    if(!head){
+        Node* node = new Node(d);
+        head=node;
+        return;
+    }
+    Node* node = new Node(d);
+    Node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=node;
+}
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        Node* head=new Node(arr[0]);
+        Node* temp=head;
+        for(int i=1;i<arr.size();i++){
+            Node* node=new Node(arr[i]);
+            temp->next=node;
+            temp=node;
+        }
+        return head;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        Node* ans = obj.constructLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
