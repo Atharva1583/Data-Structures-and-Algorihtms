92692cfee2ba260b87f1a3a2db8e88ad3949a598
Atharva Joshi <86041881+Atharva1583@users.noreply.github.com>
2023-06-06 15:09:05 +0530

    Create Insertion_In_A_Linked_List.cpp

---
+++ LinkedList/Insertion_In_A_Linked_List.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+  }  
+  cout<<"\n";
+} 
+
+
+
+// } Driver Code Ends
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution{
+  public:
+    //Function to insert a node at the beginning of the linked list.
+    Node *insertAtBegining(Node *head, int x) {
+       // Your code here
+       if(head==NULL){
+           Node* node=new Node(x);
+           head=node;
+           return head;
+       }
+       Node* node= new Node(x);
+       node->next=head;
+       head=node;
+       return head;
+    }
+    
+    
+    //Function to insert a node at the end of the linked list.
+    Node *insertAtEnd(Node *head, int x)  {
+       // Your code here
+       if(head==NULL){
+           Node* node=new Node(x);
+           head=node;
+           return head;
+       }
+       Node* temp=head;
+       Node* node=new Node(x);
+       while(temp->next!=NULL){
+           temp=temp->next;
+       }
+       temp->next=node;
+       return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+// Problem Link: https://practice.geeksforgeeks.org/problems/linked-list-insertion-1587115620/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=linked-list-insertion
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        struct Node *head = NULL;
+        for (int i = 0; i < n; ++i)
+        {
+            int data, indicator;
+            cin>>data;
+            cin>>indicator;
+            Solution obj;
+            if(indicator)
+                head = obj.insertAtEnd(head, data); 
+            else
+                head = obj.insertAtBegining(head, data);
+        }
+        printList(head); 
+    }
+    return 0; 
+} 
+
+
+// } Driver Code Ends
